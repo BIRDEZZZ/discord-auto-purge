d12cddde2ee43f187d034246634ccb707fa38dc5
wingshow <78945353+BIRDEZZZ@users.noreply.github.com>
2024-10-14 13:35:05 -0600

    Update autoPurge.js

--- autoPurge.js
+++ autoPurge.js
@@ -19,6 +19,9 @@ const deleteMessages = async (channelIDs, delay) => {
 	console.log(chalk.blue(`Checking channels for messages every ${delay / 1000} seconds...`));
 
 	setInterval(async () => {
+		const now = Date.now();
+		const cutoffTime = now - delay; 
+
 		for (const channelID of channelIDs) {
 			const channel = await client.channels.fetch(channelID).catch(err => {
 				console.error(chalk.red(`Failed to fetch channel ${channelID}: ${err.message}`));
@@ -26,7 +29,6 @@ const deleteMessages = async (channelIDs, delay) => {
 
 			if (!channel) continue;
 
-			const now = Date.now();
 			const messages = await channel.messages.fetch({
 				limit: 100
 			}).catch(err => {
@@ -35,7 +37,10 @@ const deleteMessages = async (channelIDs, delay) => {
 
 			if (!messages) continue;
 
-			const myMessages = messages.filter(msg => msg.author.id === client.user.id && (now - msg.createdTimestamp) <= delay);
+			const myMessages = messages.filter(msg => 
+				msg.author.id === client.user.id && 
+				msg.createdTimestamp < cutoffTime 
+			);
 
 			myMessages.forEach((message, index) => {
 				setTimeout(async () => {
@@ -43,7 +48,7 @@ const deleteMessages = async (channelIDs, delay) => {
 						console.error(chalk.red(`Failed to delete message ${message.id}: ${err.message}`));
 					});
 					console.log(chalk.green(`Deleted message ${message.id} from channel ${channelID}`));
-				}, 2500 * index);
+				}, 3500 * index);
 			});
 		}
 	}, delay);
